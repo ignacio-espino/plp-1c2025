18bbcefc1ef1a68447d37c32f09a248e8e7e427f
iespino <iespino@eryxsoluciones.com.ar>
2025-04-05 16:37:20 -0300

    WIP add foldDoc, <+>, indentar

--- tp1/src/Documento.hs
+++ tp1/src/Documento.hs
@@ -28,18 +28,29 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
--- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
-foldDoc = error "PENDIENTE: Ejercicio 1"
+foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
+foldDoc cVacio cTexto cLinea d = case d of
+    Vacio -> cVacio
+    Texto t d' -> cTexto t (rec d')
+    Linea i d' -> cLinea i (rec d')
+    where
+        rec = foldDoc cVacio cTexto cLinea
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = error "PENDIENTE: Ejercicio 2"
+d1 <+> d2 = foldDoc d2 combinarTexto Linea d1
+  where
+    combinarTexto t Vacio = Texto t Vacio
+    combinarTexto t (Texto t' d) = Texto (t ++ t') d
+    combinarTexto t d = Texto t d
+
+--indentar 2 (texto "a" <+> linea <+> texto "b" <+> linea <+> texto "c")
 
 indentar :: Int -> Doc -> Doc
-indentar i = error "PENDIENTE: Ejercicio 3"
+indentar i d = foldDoc Vacio (\s d' -> Texto s d') (\indPrev d' -> Linea (indPrev + i) d') d
 
 mostrar :: Doc -> String
 mostrar = error "PENDIENTE: Ejercicio 4"
