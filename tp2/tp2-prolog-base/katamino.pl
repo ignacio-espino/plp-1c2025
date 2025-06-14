186c04aeb18245bee6449160663a8cc31d3d71e7
iespino <iespino@eryx.co>
2025-06-14 14:53:17 -0300

    Agrego hasta ejercicio 4

--- tp2/tp2-prolog-base/katamino.pl
+++ tp2/tp2-prolog-base/katamino.pl
@@ -1,3 +1,22 @@
 :- use_module(piezas).
 
 % Completar ...
+
+% -- Ejercicio 1 --
+
+% sublista(+Descartar, +Tomar, +L, -R)
+
+sublista(D, T, Lista, R) :- append(A, B, Lista), length(A, D), append(R, _, B), length(R, T).
+
+length_k(K, L) :- length(L, K).
+
+% tablero(+K, -T)
+tablero(K, T) :- length(T, 5), maplist(length_k(K), T).
+
+% tamano(+M, -F, -C)
+tamano(M, F, C) :- length(M, F), maplist(length_k(C), M).
+
+% coordenadas(+T, -IJ)
+
+% coordenadas(+T, -IJ)
+coordenadas(T, (I,J)) :- tamano(T, F, C), between(1, F, I), between(1, C, J).
