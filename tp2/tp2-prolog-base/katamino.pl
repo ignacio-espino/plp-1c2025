a73d5032822eea3c19e79630ae45be4c36866f27
iespino <iespino@eryx.co>
2025-06-14 14:55:13 -0300

    formateo archivo

--- tp2/tp2-prolog-base/katamino.pl
+++ tp2/tp2-prolog-base/katamino.pl
@@ -1,6 +1,5 @@
 :- use_module(piezas).
 
-% Completar ...
 
 % -- Ejercicio 1 --
 
@@ -8,15 +7,22 @@
 
 sublista(D, T, Lista, R) :- append(A, B, Lista), length(A, D), append(R, _, B), length(R, T).
 
+
+% -- Ejercicio 2 --
+
 length_k(K, L) :- length(L, K).
 
 % tablero(+K, -T)
 tablero(K, T) :- length(T, 5), maplist(length_k(K), T).
 
+
+% -- Ejercicio 3 --
+
 % tamano(+M, -F, -C)
 tamano(M, F, C) :- length(M, F), maplist(length_k(C), M).
 
-% coordenadas(+T, -IJ)
+
+% -- Ejercicio 4 --
 
 % coordenadas(+T, -IJ)
 coordenadas(T, (I,J)) :- tamano(T, F, C), between(1, F, I), between(1, C, J).
